596463b4925a9020164e2fa121bdc9f689ed7e65
Joseph B. Ottinger <joseph.ottinger@meltwater.com>
2018-02-18 11:16:34 -0500

    Beginning actual project layout

---
+++ jvmmodel/src/main/java/model/EventSource.java
@@ -0,0 +1,27 @@
+package com.enigmastation.techstream.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@MappedSuperclass
+public class EventSource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    String name;
+    /* might change, because this is a discriminator field */
+    String type;
+    double weight;
+    Duration frequency;
+    String description;
+    boolean active;
+    Instant lastReadTime;
+    double churn;
+    int relevance;
+}
